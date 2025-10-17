3cf32e2bb1e2c3dc2c8c4823329ef0b650dc970e
David Pamplona <davypamp@gmail.com>
2025-10-17 07:11:32 -0300

    Iniciando projeto do portal câmara

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// theme: {
+//   extend: {
+//     colors: {
+//       primary: "var(--color-primary)",
+//       secondary: "var(--color-secondary)",
+//       accent: "var(--color-accent)",
+//       dark: "var(--color-dark)",
+//       light: "var(--color-light)",
+//       text: "var(--color-text)",
+//       "text-light": "var(--color-text-light)",
+//     },
+//   },
+// }
